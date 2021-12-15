722cd7edeed4797073989a6fdd2134b1a2b4e986
Forntoh Thomas <thomasforntoh@gmail.com>
2021-12-16 00:36:51 +0100

    typwriter remove

--- components/sections/home.js
+++ components/sections/home.js
@@ -1,14 +1,9 @@
-import TypeWriter from "react-typewriter";
-
 function Home({ data = { name: "Forntoh Thomas" } }) {
   return (
     <div className="section container">
       <div className="space-y-6">
         <p>Hi, welcome to my world;</p>
-        <h1>
-          <TypeWriter typing={0.75}>{`I'm ${data?.name}.`}</TypeWriter>
-        </h1>
-
+        <h1>{`I'm ${data?.name}.`}</h1>
         <p className="max-w-2xl">
           {`I’m a software engineer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
